83855d30efa5cb3c890da70946e7e012e92c190f
shirls81 <blond-shirls@hotmail.com>
2016-02-02 09:23:37 -0700

    finished quiz 2
    Finished yo.

--- user.js
+++ user.js
@@ -1,7 +1,41 @@
 'use strict';
 
-function User(username, password) {
+var assert = require('assert');
+var bcrypt = require('bcrypt');
 
+var users = [];
+
+function User(username, password) {
+  var userExists = User.find(username);
+
+  assert(!userExists, 'Username is already in use.');
+
+  this.username = username;
+  this.password = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+
+  users.push(this);
+};
+
+User.prototype.authenticate = function(password){
+  return bcrypt.compareSync(password, this.password);
+};
+
+User.find = function(username){
+    var foundUser = users.find(function(user){
+      return user.username === username;
+    });
+return foundUser || null;
+
+};
+
+User.authenticate = function(username, password){
+  var user = User.find(username);
+
+  if (!user) { return false;}
+  var validPassword = user.authenticate(password);
+
+  if (!validPassword) {return false;}
+  return user;
 }
 
 module.exports = User;
